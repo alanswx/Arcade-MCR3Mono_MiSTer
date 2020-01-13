a6c01da4e144aa8f17b7a50dadf8da9b28feeff4
Alan Steremberg <alan.steremberg@gmail.com>
2020-01-13 12:08:46 -0800

    fixes

--- Arcade-MCR3Mono.sv
+++ Arcade-MCR3Mono.sv
@@ -157,12 +157,11 @@ wire        ioctl_download;
 wire        ioctl_wr;
 wire [24:0] ioctl_addr;
 wire  [7:0] ioctl_dout;
+wire  [7:0] ioctl_index;
 
 wire [10:0] ps2_key;
 
-wire [15:0] joystick_0, joystick_1;
-wire [15:0] joy = joystick_0 | joystick_1;
-wire [15:0] joy_a;
+wire [15:0] joystick_0, joystick_1,joystick_2,joystick_3;
 
 wire [21:0] gamma_bus;
 
@@ -184,16 +183,34 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.ioctl_wr(ioctl_wr),
 	.ioctl_addr(ioctl_addr),
 	.ioctl_dout(ioctl_dout),
+	.ioctl_index(ioctl_index),
 
 	.joystick_0(joystick_0),
 	.joystick_1(joystick_1),
-	.joystick_analog_0(joy_a),
+	.joystick_2(joystick_2),
+	.joystick_3(joystick_3),
+
 	.ps2_key(ps2_key)
 );
 
+reg mod_rampage    = 0;
+reg mod_sarge      = 0;
+reg mod_powerdrive = 0;
+reg mod_maxrpm     = 0;
+
+always @(posedge clk_sys) begin
+        reg [7:0] mod = 0;
+        if (ioctl_wr & (ioctl_index==1)) mod <= ioctl_dout;
+
+	mod_rampage	<= (mod == 0);
+	mod_sarge	<= (mod == 1);
+	mod_powerdrive	<= (mod == 2);
+	mod_maxrpm	<= (mod == 3);
+end
 
 
-`define CORE_NAME "RAMPAGE"
+wire service=status[6];
+
 reg       sg; // Sounds Good board
 reg [7:0] input0;
 reg [7:0] input1;
@@ -204,9 +221,9 @@ reg [7:0] output5;
 reg [7:0] output6;
 
 // Game specific sound board/DIP/input settings
+// Game specific sound board/DIP/input settings
 always @(*) begin
-        if (`CORE_NAME == "SARGE" ||
-            `CORE_NAME == "MAXRPM")
+        if (mod_sarge | mod_maxrpm )
                 sg = 0;
         else
                 sg = 1;
@@ -216,30 +233,30 @@ always @(*) begin
         input2 = 8'hff;
         input3 = 8'hff;
         input4 = 8'hff;
-/*
-        if (`CORE_NAME == "SARGE") begin
+
+        if (mod_sarge  ) begin
                 // Two stick/player like the original
-                input0 = ~{2'b00, service, 1'b0, m_two_players, m_one_player, m_coin2, m_coin1};
-                input1 = ~{m_fireA | m_fireB, m_fireA | m_fireB, m_fire2A | m_fire2B, m_fire2A | m_fire2B, m_down, m_up, m_down, m_up};
-                input2 = ~{m_fire3A | m_fire3B, m_fire3A | m_fire3B, m_fire4A | m_fire4B, m_fire4A | m_fire4B, m_down3, m_up3, m_down4, m_up4};
-        end else if (`CORE_NAME == "MAXRPM") begin
-                input0 = ~{service, 3'b000, m_one_player, m_two_players, m_coin1, m_coin2};
-                input1 = ~{maxrpm_adc_data};
+                input0 = ~{2'b00, service, 1'b0, m_start2, m_start1, m_coin2, m_coin1};
+                input1 = ~{m_fire1 | m_fire1b, m_fire1 | m_fire1b, m_fire2 | m_fire2b, m_fire2 | m_fire2b, m_down1, m_up1, m_down2, m_up2};
+                input2 = ~{m_fire3 | m_fire3b, m_fire3 | m_fire3b, m_fire4 | m_fire4b, m_fire4 | m_fire4b, m_down3, m_up3, m_down4, m_up4};
+	end else if (mod_maxrpm  ) begin
+                input0 = ~{service, 3'b000, m_start1, m_start2, m_coin1, m_coin2};
+                input1 =  {pedal1[5:2], pedal2[5:2]};
                 input2 = ~{maxrpm_gear1, maxrpm_gear2};
-        end else if (`CORE_NAME == "RAMPAGE") begin
+	end else if (mod_rampage ) begin
                 // normal controls for 3 players
                 input0 = ~{2'b00, service, 1'b0, 2'b00, m_coin2, m_coin1};
-                input1 = ~{2'b00, m_fireB, m_fireA, m_left, m_down, m_right, m_up};
-                input2 = ~{2'b00, m_fire2B, m_fire2A, m_left2, m_down2, m_right2, m_up2};
-                input4 = ~{2'b00, m_fire3B, m_fire3A, m_left3, m_down3, m_right3, m_up3};
-        end else if (`CORE_NAME == "POWERDRV") begin
+                input1 = ~{2'b00, m_fire1b, m_fire1, m_left1, m_down1, m_right1, m_up1};
+                input2 = ~{2'b00, m_fire2b, m_fire2, m_left2, m_down2, m_right2, m_up2};
+                input4 = ~{2'b00, m_fire3b, m_fire3, m_left3, m_down3, m_right3, m_up3};
+	end else if (mod_powerdrive ) begin
                 // Controls for 3 players using 4 buttons/joystick
                 input0 = ~{2'b00, service, 1'b0, 1'b0, m_coin3, m_coin2, m_coin1};
-                input1 = ~{m_fire2B, m_fire2A, powerdrv_gear[1], m_fire2C, m_fireB, m_fireA, powerdrv_gear[0], m_fireC};
-                input2 = ~{sndstat[0], 3'b000, m_fire3B, m_fire3A, powerdrv_gear[2], m_fire3C};
+                input1 = ~{m_fire2b, m_fire2, powerdrv_gear[1], m_fire2c, m_fire1b, m_fire1, powerdrv_gear[0], m_fire1c};
+                input2 = ~{sndstat[0], 3'b000, m_fire3b, m_fire3, powerdrv_gear[2], m_fire3c};
         end
-*/
 end
+
 
 wire [15:0] rom_addr;
 wire [15:0] rom_do;
@@ -387,58 +404,113 @@ always @(posedge clk_sys) begin
 	
 	if(old_state != ps2_key[10]) begin
 		casex(code)
-			'hX75: btn_up          <= pressed; // up
-			'hX72: btn_down        <= pressed; // down
-			'hX6B: btn_left        <= pressed; // left
-			'hX74: btn_right       <= pressed; // right
-			'h029: btn_fire1       <= pressed; // space
-			'h014: btn_fire3       <= pressed; // ctrl
-
-			'h004: btn_coin        <= pressed; // F3
-
-			// JPAC/IPAC/MAME Style Codes
-			'h02E: btn_coin        <= pressed; // 5
-			'h036: btn_coin        <= pressed; // 6
-			'h02D: btn_up          <= pressed; // R
-			'h02B: btn_down        <= pressed; // F
-			'h023: btn_left        <= pressed; // D
-			'h034: btn_right       <= pressed; // G
-			'h01C: btn_fire1       <= pressed; // A
-
-			'h00D: btn_van         <= pressed; // TAB
-			'h01A: btn_shift       <= pressed; // Z
-			'h012: btn_fire4       <= pressed; // shift left
-			'h011: btn_fire2       <= pressed; // alt
+                        'h75: btn_up            <= pressed; // up
+                        'h72: btn_down          <= pressed; // down
+                        'h6B: btn_left          <= pressed; // left
+                        'h74: btn_right         <= pressed; // right
+                        'h76: btn_coin          <= pressed; // ESC
+                        'h05: btn_one_player    <= pressed; // F1
+                        'h06: btn_two_players   <= pressed; // F2
+                        'h04: btn_three_players <= pressed; // F3
+                        'h0C: btn_four_players  <= pressed; // F4
+                        'h12: btn_fireD         <= pressed; // l-shift
+                        'h14: btn_fireC         <= pressed; // ctrl
+                        'h11: btn_fireB         <= pressed; // alt
+                        'h29: btn_fireA         <= pressed; // Space
+                        // JPAC/IPAC/MAME Style Codes
+                        'h16: btn_start1_mame   <= pressed; // 1
+                        'h1E: btn_start2_mame   <= pressed; // 2
+                        'h26: btn_start3_mame   <= pressed; // 3
+                        'h25: btn_start4_mame   <= pressed; // 4
+                        'h2E: btn_coin1_mame    <= pressed; // 5
+                        'h36: btn_coin2_mame    <= pressed; // 6
+                        'h3D: btn_coin3_mame    <= pressed; // 7
+                        'h3E: btn_coin4_mame    <= pressed; // 8
+                        'h2D: btn_up2           <= pressed; // R
+                        'h2B: btn_down2         <= pressed; // F
+                        'h23: btn_left2         <= pressed; // D
+                        'h34: btn_right2        <= pressed; // G
+                        'h1C: btn_fire2A        <= pressed; // A
+                        'h1B: btn_fire2B        <= pressed; // S
+                        'h21: btn_fire2C        <= pressed; // Q
+                        'h1D: btn_fire2D        <= pressed; // W
 		endcase
 	end
 end
 
-reg btn_van   = 0;
-reg btn_shift = 0;
-reg btn_fire1 = 0;
-reg btn_fire2 = 0;
-reg btn_fire3 = 0;
-reg btn_fire4 = 0;
-
-
-reg btn_up    = 0;
-reg btn_down  = 0;
+reg btn_one_player = 0;
+reg btn_two_players = 0;
+reg btn_three_players = 0;
+reg btn_four_players = 0;
+reg btn_left = 0;
 reg btn_right = 0;
-reg btn_left  = 0;
+reg btn_down = 0;
+reg btn_up = 0;
+reg btn_fireA = 0;
+reg btn_fireB = 0;
+reg btn_fireC = 0;
+reg btn_fireD = 0;
 reg btn_coin  = 0;
+reg btn_start1_mame = 0;
+reg btn_start2_mame = 0;
+reg btn_start3_mame = 0;
+reg btn_start4_mame = 0;
+reg btn_coin1_mame = 0;
+reg btn_coin2_mame = 0;
+reg btn_coin3_mame = 0;
+reg btn_coin4_mame = 0;
+reg btn_up2 = 0;
+reg btn_down2 = 0;
+reg btn_left2 = 0;
+reg btn_right2 = 0;
+reg btn_fire2A = 0;
+reg btn_fire2B = 0;
+reg btn_fire2C = 0;
+reg btn_fire2D = 0;
 
-wire m_up     = no_rotate ? btn_left  | joy[1] : btn_up    | joy[3];
-wire m_down   = no_rotate ? btn_right | joy[0] : btn_down  | joy[2];
-wire m_left   = no_rotate ? btn_down  | joy[2] : btn_left  | joy[1];
-wire m_right  = no_rotate ? btn_up    | joy[3] : btn_right | joy[0];
+// Generic controls - make a module from this?
+wire m_coin1   = btn_coin1_mame  | btn_coin;
+wire m_start1  = btn_start1_mame | btn_one_player;
+wire m_up1     = btn_up    | joystick_0[3];
+wire m_down1   = btn_down  | joystick_0[2];
+wire m_left1   = btn_left  | joystick_0[1];
+wire m_right1  = btn_right | joystick_0[0];
+wire m_fire1   = btn_fireA | joystick_0[4];
+wire m_fire1b  = btn_fireB | joystick_0[5];
+wire m_fire1c  = btn_fireC | joystick_0[6];
+wire m_fire1d  = btn_fireD | joystick_0[7];
 
-wire m_fire1  = btn_fire1 | joy[4];
-wire m_fire2  = btn_fire2 | joy[5];
-wire m_fire3  = btn_fire3 | joy[6];
-wire m_fire4  = btn_fire4 | joy[7];
-wire m_van    = btn_van   | joy[8];
-wire m_shift  = btn_shift | joy[9];
-wire m_coin   = btn_coin  | joy[10];
+wire m_coin2   = btn_coin2_mame  | btn_coin;
+wire m_start2  = btn_start2_mame | btn_two_players;
+wire m_left2   = btn_left2  | joystick_1[1];
+wire m_right2  = btn_right2 | joystick_1[0];
+wire m_up2     = btn_up2    | joystick_1[3];
+wire m_down2   = btn_down2  | joystick_1[2];
+wire m_fire2   = btn_fire2A | joystick_1[4];
+wire m_fire2b  = btn_fire2B | joystick_1[5];
+wire m_fire2c  = btn_fire2C | joystick_1[6];
+wire m_fire2d  = btn_fire2D | joystick_1[7];
+
+wire m_coin3   = btn_coin3_mame  | btn_coin;
+wire m_start3  = btn_start3_mame | btn_three_players;
+wire m_left3   = joystick_2[1];
+wire m_right3  = joystick_2[0];
+wire m_up3     = joystick_2[3];
+wire m_down3   = joystick_2[2];
+wire m_fire3   = joystick_2[4];
+wire m_fire3b  = joystick_2[5];
+wire m_fire3c  = joystick_2[6];
+wire m_fire3d  = joystick_2[7];
+
+wire m_coin4   = btn_coin4_mame  | btn_coin;
+wire m_start4  = btn_start4_mame | btn_four_players;
+wire m_left4   = joystick_3[1];
+wire m_right4  = joystick_3[0];
+wire m_up4     = joystick_3[3];
+wire m_down4   = joystick_3[2];
+wire m_fire4   = joystick_3[4];
+wire m_fire4b  = joystick_3[5];
+
 
 wire ce_pix_old;
 
@@ -470,22 +542,6 @@ arcade_fx #(512,9) arcade_video//505?
 
         .fx(status[5:3])
 );
-/*
-// 512x480
-arcade_rotate_fx #(496,240,9) arcade_video
-(
-	.*,
-
-	.clk_video(clk_sys),
-	.RGB_in({r,g,b}),
-	.HBlank(hblank),
-	.VBlank(vblank),
-	.HSync(hs),
-	.VSync(vs),
-
-	.fx(status[5:3])
-);
-*/
 
 assign AUDIO_S = 0;
 //wire [15:0] audio_l, audio_r;
@@ -494,41 +550,72 @@ wire  [9:0] audio;
 
 assign AUDIO_L = { audio, 5'd0 };
 assign AUDIO_R = { audio, 5'd0 };
-/*
-mcr3mono mcr3mono (
+
+
+// MaxRPM gearbox
+wire [3:0] maxrpm_gear_bits[5] = '{ 4'h0, 4'h5, 4'h6, 4'h1, 4'h2 };
+wire [3:0] maxrpm_gear1 = maxrpm_gear_bits[gear1];
+wire [3:0] maxrpm_gear2 = maxrpm_gear_bits[gear2];
+reg  [2:0] gear1;
+reg  [2:0] gear2;
+always @(posedge clk_sys) begin
+        reg m_fire1_last, m_fire1b_last;
+        reg m_fire2_last, m_fire2b_last;
+
+        if (reset) begin
+                gear1 <= 0;
+                gear2 <= 0;
+        end else begin
+                m_fire1_last <= m_fire1;
+                m_fire1b_last <= m_fire1b;
+                m_fire2_last <= m_fire2;
+                m_fire2b_last <= m_fire2b;
+
+                if (m_start1) gear1 <= 0;
+                else if (~m_fire1_last && m_fire1 && gear1 != 3'd4) gear1 <= gear1 + 1'd1;
+                else if (~m_fire1b_last && m_fire1b && gear1 != 3'd0) gear1 <= gear1 - 1'd1;
+
+                if (m_start2) gear2 <= 0;
+                else if (~m_fire2_last && m_fire2 && gear2 != 3'd4) gear2 <= gear2 + 1'd1;
+                else if (~m_fire2b_last && m_fire2b && gear2 != 3'd0) gear2 <= gear2 - 1'd1;
+        end
+end
+
+// Power Drive gear
+reg  [2:0] powerdrv_gear;
+always @(posedge clk_sys) begin
+        reg [2:0] gear_old;
+        if (reset) powerdrv_gear <= 0;
+        else begin
+                gear_old <= {m_fire3d, m_fire2d, m_fire1d};
+                if (~gear_old[0] & m_fire1d) powerdrv_gear[0] <= ~powerdrv_gear[0];
+                if (~gear_old[1] & m_fire2d) powerdrv_gear[1] <= ~powerdrv_gear[1];
+                if (~gear_old[2] & m_fire3d) powerdrv_gear[2] <= ~powerdrv_gear[2];
+        end
+end
+
+//Pedals for Max RPM
+wire [5:0] pedal1;
+spinner spinner1 (
         .clock_40(clk_sys),
         .reset(reset),
-        .video_r(r),
-        .video_g(g),
-        .video_b(b),
-        .video_blankn(blankn),
-        .video_hs(hs),
-        .video_vs(vs),
-        .video_csync(cs),
-        .tv15Khz_mode(scandoublerD),
-
-        .soundsgood(sg),
-        .snd_stat(sndstat),
-        .audio_out(audio),
-
-        .input_0(input0),
-        .input_1(input1),
-        .input_2(input2),
-        .input_3(input3),
-        .input_4(input4),
-        .output_5(output5),
-        .output_6(output6),
-
-        .cpu_rom_addr ( rom_addr        ),
-        .cpu_rom_do   ( rom_addr[0] ? rom_do[15:8] : rom_do[7:0] ),
-        .snd_rom_addr ( snd_addr        ),
-        .snd_rom_do   ( snd_do          ),
-        .sp_addr      ( sp_addr         ),
-        .sp_graphx32_do ( sp_do         ),
-
-        .dl_addr(ioctl_addr-gfx1_offset),
-        .dl_data(ioctl_dout),
-        .dl_wr(ioctl_wr)
+        .btn_acc(),
+        .btn_left(m_up1),
+        .btn_right(m_down1),
+        .ctc_zc_to_2(vs),
+        .spin_angle(pedal1)
 );
-*/
+
+wire [5:0] pedal2;
+spinner spinner2 (
+        .clock_40(clk_sys),
+        .reset(reset),
+        .btn_acc(),
+        .btn_left(m_up2),
+        .btn_right(m_down2),
+        .ctc_zc_to_2(vs),
+        .spin_angle(pedal2)
+);
+
+
 endmodule
